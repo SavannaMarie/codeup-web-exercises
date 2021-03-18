6bfb69756f33d36d4b1b279823fb955f482df1e5
Savanna Marie <savannamarie@gmail.com>
2021-03-18 15:33:18 -0500

    completed the js with HTML lecture

---
+++ js/external.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// var message = "Hello!"
+// var whatIsThisThen = "Have a great day"
+// var favoriteCheese = prompt("What is your favorite cheese?");
+//
+//
+// console.log(favoriteCheese + " happens to be their favorite cheese!");
+// var typeOfPrompt = typeof prompt("Tell me something!");
+// console.log(typeOfPrompt);
+//
+//
+// var num = parseFloat(prompt("Give me a number..."));
+// console.log("User entered: " + num + ". That number plus 17 is: " + (num + 17) + ".")
+//
+// var canIGoToTRS = confirm("Can I go to toys r us please?");
+// console.log(canIGoToTRS);
+
+alert("You're looking at a website!");
