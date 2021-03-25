66ec13d2c64abe4a8971c97d9d54660b05c9d378
Savanna Marie <savannamarie@gmail.com>
2021-03-25 11:14:19 -0500

    loops exercise

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -21,19 +21,41 @@
 //Tell the user how to end the program (sentinel value)
 //Loop back around and do it all again, over and over until the user tells it to stop
 
-var priceOfItem = parseFloat(prompt("Enter the price of your first item: "));
-alert("The price of your item was " + priceOfItem);
-//create accumulator variable
-var totalCost = priceOfItem;
-var userInput = "";
-//Create a sentinel value
-while(userInput !== "STOP") {
-    userInput = (prompt("Enter the price of your next item: \r\n\ Enter STOP when you are done"));
-    if (userInput === "STOP"){
-        alert("OK. Your final total is " + totalCost);
-    } else {
-        priceOfItem = parseFloat(userInput);
-        totalCost = totalCost + priceOfItem;
-        alert("Your total is now " + totalCost.toFixed(2));
-    }
+// var priceOfItem = parseFloat(prompt("Enter the price of your first item: "));
+// alert("The price of your item was " + priceOfItem);
+// //create accumulator variable
+// var totalCost = priceOfItem;
+// var userInput = "";
+// //Create a sentinel value
+// while(userInput !== "STOP") {
+//     userInput = (prompt("Enter the price of your next item: \r\n\ Enter STOP when you are done"));
+//     if (userInput === "STOP"){
+//         alert("OK. Your final total is " + totalCost);
+//     } else {
+//         priceOfItem = parseFloat(userInput);
+//         totalCost = totalCost + priceOfItem;
+//         alert("Your total is now " + totalCost.toFixed(2));
+//     }
+// }
+
+var counter = 8;
+while (counter >= 0){
+    console.log(counter);
+    counter = counter - 1;
 }
+
+//PseudoCode
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/while.js
@@ -0,0 +1,6 @@
+var numMultiply = 2;
+
+while (numMultiply <= 65536) {
+    console.log(numMultiply);
+    numMultiply = numMultiply * 2;
+}
