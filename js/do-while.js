812d08da568c403fe210b64655f3a66a79968038
Savanna Marie <savannamarie@gmail.com>
2021-03-25 16:41:57 -0500

    do-while exercise update

--- js/do-while.js
+++ js/do-while.js
@@ -6,6 +6,17 @@ do {
     console.log("There are currently " + allCones + " cones for purchase!");
     allCones = allCones - conesSold;
     conesSold = Math.floor(Math.random() * 5) + 1;
-    console.log(allCones);
+    console.log("we sold " + conesSold);
 } while (conesSold <= allCones)
     console.log("There are " + allCones + " left for purchase");
+
+
+// do {
+//     var conesBought = Math.floor(Math.random() * 5) + 1;
+//     if (conesBought > allCones) {
+//         console.log("I cant sell " + conesBought + " I only have  " + allCones + " left sorry");
+//     } else {
+//         allCones = allCones - conesBought;
+//         console.log("Cones Sold " + conesBought + " I have " + allCones + " left to sell");
+//     }
+// } while (allCones > 0);
