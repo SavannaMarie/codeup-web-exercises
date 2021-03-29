0583a127cc04924423441b24526d549ad2e1ff4a
Savanna Marie <savannamarie@gmail.com>
2021-03-29 16:54:44 -0500

    iterating exercise

--- js/iterating.js
+++ js/iterating.js
@@ -6,29 +6,44 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    var names = ["Mark", "Marky", "Savanna", "Killer"];
+
+
+
 
     /**
+     *
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+console.log(names);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+for (var i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
+    names.forEach(function (name){
+        console.log(name);
+    });
+
 
     /**
      * TODO:
@@ -43,5 +58,20 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-
+    function returnFirst(array) {
+        return array[0];
+    }
+    function returnSecond(array) {
+        return array[1];
+    }
+    function returnThird(array) {
+        return array[2];
+    }
+    function returnLast(array) {
+        return array[3];
+    }
+    console.log(returnThird(names));
+    console.log(returnSecond(names));
+    console.log(returnThird(names));
+    console.log(returnLast(names));
 })();
