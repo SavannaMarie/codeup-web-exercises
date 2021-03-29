cd3cda8f3c1a186d9a6d252347fdaa4df0afc737
Savanna Marie <savannamarie@gmail.com>
2021-03-29 10:26:53 -0500

    going over loops exercises

--- js/for-loops.js
+++ js/for-loops.js
@@ -1,8 +1,30 @@
+"use strict";
 
 
-function showMultiplicationTable(i) {
-    for (var i = 0; i < 11; i++) {
-        console.log(i * 7);
+// function showMultiplicationTable(i) {
+//     for (var i = 1; i < 11; i++) {
+//         console.log("7 x " + i + " = " + (i * 7));
+//     }
+// }
+// showMultiplicationTable();
+//
+
+
+
+for(var i = 0; i < 10; i++) {
+    var random = Math.floor(Math.random() * 181 + 20);
+    if (random % 2 === 0) {
+        console.log(random + " is even!");
+    } else {
+        console.log(random + " is odd");
     }
 }
-showMultiplicationTable();
+
+
+
+
+
+
+
+
+
