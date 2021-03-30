7d38f53c73b08eefd603680afab9eff912b78f4a
Savanna Marie <savannamarie@gmail.com>
2021-03-30 16:00:09 -0500

    objects lecture notes

---
+++ js/objects-lec.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var tacoma = {
+    make: "Toyota",
+    model: "Tacoma",
+    truck: true,
+    seat: 5,
+    color: "Blue",
+    mileage: 10,
+    drive: function(){
+        console.log("VROOM VROOM!!");
+        this.mileage++
+    }
+
+}
