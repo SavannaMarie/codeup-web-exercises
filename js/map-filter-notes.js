d0abbf5d595d3a511007dee80e933590050fb62c
Savanna Marie <savannamarie@gmail.com>
2021-05-17 09:58:37 -0500

    reduce notes

--- js/map-filter-notes.js
+++ js/map-filter-notes.js
@@ -32,36 +32,54 @@ let output = '';
 let prices = [32.99, 11.99, 6.96, 45.32, 8.98, 5.99];
 let pricesAfterTax = [];
 
-// pricesAfterTax = prices.map(function (price){
-//    let tax = (price * 0.0825).toFixed(2);
-//    let total = (parseFloat(price) + parseFloat(tax)).toFixed(2);
-//    return parseFloat(total);
-// });
+pricesAfterTax = prices.map(function (price){
+   let tax = (price * 0.0825).toFixed(2);
+   let total = (parseFloat(price) + parseFloat(tax)).toFixed(2);
+   return parseFloat(total);
+});
 //
 // pricesAfterTax.forEach(function(item) {
 //     output = output + "<p>" + item + "</p>";
 //     $("#output").html(output);
 // });
 
-let things = ['book', 'pencil', 'eraser'];
-let pluralThings = things.map((thing, index) =>{
-    return thing;
+// let things = ['book', 'pencil', 'eraser'];
+// let pluralThings = things.map((thing, index) =>{
+//     return thing;
+// });
+//
+// pluralThings.forEach(function (thing, index){
+//     if (index === 0) {
+//         output = `<p>You must bring the following items: </p>`
+//     }
+//         output += `<p>${thing}s</p>`;
+//     $('#output').html(output);
+//     return thing;
+// });
+
+
+//=====Reduce
+
+let totalCost = pricesAfterTax.reduce(function (total, itemPrice, index){
+    console.log(`The index is: ${index} the total is: ${total}, the item price is: ${itemPrice}`)
+    return total + itemPrice;
+
 });
 
-pluralThings.forEach(function (thing, index){
-    if (index === 0) {
-        output = `<p>You must bring the following items: </p>`
+var averagePrice = pricesAfterTax.reduce(function (total, itemPrice, index, array) {
+    total = total + itemPrice;
+    if (index === array.length - 1) {
+        return total/array.length;
+    } else {
+        return total;
     }
-        output += `<p>${thing}s</p>`;
-    $('#output').html(output);
-    return thing;
+
 });
 
+console.log(averagePrice.toFixed(2));
 
-
-
-
-
-
-
-
+totalCost = prices.reduce(function (total, price, index) {
+    console.log(`The index is: ${index} the total is: ${total}, the item price is: ${price}`)
+    return total + price;
+}, 158.76);
+console.log(totalCost.toFixed(2))
