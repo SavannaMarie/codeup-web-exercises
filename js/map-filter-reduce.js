a86e46c2b172510ab8781082183ec033dbbc089d
Savanna Marie <savannamarie@gmail.com>
2021-05-17 11:24:46 -0500

    reduce exercise"

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,8 +36,61 @@ const users = [
     }
 ];
 
+
+//Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 let triLanguages = users.filter(user => user.languages.length >= 3);
 console.log(triLanguages);
 
+//Use .map to create an array of strings where each element is a user's email address
 let usersEmails = users.map(user =>  user.email);
 console.log(usersEmails)
+
+
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+
+let totalYears = users.reduce(function (total, user, index){
+    return total + itemPrice;
+
+});
+
+// let totalYears = users.reduce(function (total, user){
+//     return total + user.yearsOfExperience
+// });
+// console.log(totalYears);
+
+// var averageYears = yearsExperience.reduce(function (years, index, array) {
+//     years = years + years[index];
+//     if (index === array.length - 1) {
+//         return years/array.length;
+//     } else {
+//         return years;
+//     }
+// });
+//
+// console.log(averageYears)
+
+// Use .reduce to get the longest email from the list of users.
+
+
+
+// Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+// let instructors = users.reduce(function (name, index){
+//     return name[index];
+// });
+//
+// console.log(instructors)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
