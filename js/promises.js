13482fac20c20734fe4de63d5c3bf3eb0b895b5a
Savanna Marie <savannamarie@gmail.com>
2021-05-17 17:01:31 -0500

    promises exercise

--- js/promises.js
+++ js/promises.js
@@ -3,4 +3,4 @@ let user = 'SavannaMarie'
 fetch(`https://api.github.com/users/${user}/events/public`, {
     method: 'GET',
     headers: {'Authorization': GITHUB_TOKEN}
-}).then (resp => resp.json()).then(resp => console.log(resp))
+}).then (resp => resp.json()).then(resp => console.log(resp[0].payload.commits[0]))
