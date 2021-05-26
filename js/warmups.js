1356516f96ab8b1dfc4e95ef516e6b1cc1e3c78d
Savanna Marie <savannamarie@gmail.com>
2021-05-26 09:16:51 -0500

    morning exercise

--- js/warmups.js
+++ js/warmups.js
@@ -1,16 +1,38 @@
 "use strict";
-for (let i = 0; i <= 30; i++ ) {
-    if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log("FizzBuzz")
-    } else if (i % 3 === 0) {
-        console.log("Fizz")
-    } else if (i % 5 === 0) {
-        console.log("Buzz")
+// for (let i = 0; i <= 30; i++ ) {
+//     if ((i % 3 === 0) && (i % 5 === 0)) {
+//         console.log("FizzBuzz")
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz")
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz")
+//     } else {
+//         console.log(i);
+//     }
+// }
+//
+// //Returns number 7
+// const seven = () => console.log('7');
+// seven();
+
+//Warm up
+function five(x){
+    if (typeof x === "number") {
+        return x * 5;
     } else {
-        console.log(i);
+        return 0;
     }
 }
 
-//Returns number 7
-const seven = () => console.log('7');
-seven()
+console.log(five(5));
+console.log(five(50));
+console.log(five(-50));
+console.log(five("the"));
+console.log(five("5"));
+
+
+
+
+
+
+
