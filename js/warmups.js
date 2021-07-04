357895a35a4a224804669673ffe56adeaf73de3f
Savanna Marie <savannamarie@gmail.com>
2021-07-04 14:55:06 -0500

    JS Practice

--- js/warmups.js
+++ js/warmups.js
@@ -105,3 +105,12 @@ function animate_string(id)
         textNode.data = text;
     }, 100);
 }
+
+{
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+}
+console.log(leapyear(2016));
+console.log(leapyear(2000));
+console.log(leapyear(1700));
+console.log(leapyear(1800));
+console.log(leapyear(100));
