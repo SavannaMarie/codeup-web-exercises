1e6cf3e518b6568e7ef472c50851763a1da02299
Savanna Marie <savannamarie@gmail.com>
2021-07-31 23:12:23 -0500

    Javascript

--- js/arrays-notes.js
+++ js/arrays-notes.js
@@ -197,11 +197,11 @@
 
     // Which I can use to build.. some
 
-    // function removeAtIndex(array, index) {
-    //     var arr1 = array.slice(0, index);
-    //     var arr2 = array.slice(index + 1);
-    //     return arr1.concat(arr2);
-    // }
+    function removeAtIndex(array, index) {
+        var arr1 = array.slice(0, index);
+        var arr2 = array.slice(index + 1);
+        return arr1.concat(arr2);
+    }
 
       // console.log(removeAtIndex(cheeses, cheeses.indexOf("Kraft American Singles")));
     //
